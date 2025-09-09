248cec88c84bc7c0db02f5150b1dc8ac88cc3bd4
Brittany Halterman <halterma@gmail.com>
2025-09-09 16:30:02 -0400

    Define resourceful routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :categories
+  resources :topics
+  resources :decks
+  resources :cards
 end
