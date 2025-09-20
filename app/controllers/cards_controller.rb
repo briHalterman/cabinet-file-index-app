8ecadd9dc525afb14bc0a2a92d3b3b518566b9ad
Brittany Halterman <halterma@gmail.com>
2025-09-20 12:51:35 -0400

    Implement cards#show route

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,14 @@ class CardsController < ApplicationController
   def index
     @cards = Card.all
   end
+
+  def show
+    @card = Card.find(params[:id])
+
+    if params[:side] == "face"
+      @content = @card.face_content
+    elsif params[:side] == "back"
+      @content = @card.back_content
+    end
+  end
 end
--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -53,5 +53,74 @@ RSpec.describe "Cards", type: :request do
         expect(response.body).not_to include('<li>')
       end
     end
+
+  end
+
+  describe "GET /cards/:id?side=face" do
+      let!(:category) do
+        Category.create!(
+          title: 'Test Category'
+        )
+      end
+
+      let!(:topic) do
+        Topic.create!(
+          title: 'Test Topic',
+          category_id: category.id
+        )
+      end
+
+      let!(:deck) do
+        Deck.create!(
+          title: 'Test Deck',
+        )
+      end
+
+      let!(:card) do
+        Card.create!(
+          title: 'Test Card',
+          face_content: 'This is the content on the face (lined) of the index card.',
+          back_content: 'The back side of the card is the blank side.'
+        )
+      end
+
+      it 'returns the content on the face of the card' do
+        get "/cards/#{card.id}?side=face"
+        expect(response.body).to include('This is the content on the face (lined) of the index card.')
+      end
+  end
+
+    describe "GET /cards/:id?side=back" do
+      let!(:category) do
+        Category.create!(
+          title: 'Test Category'
+        )
+      end
+
+      let!(:topic) do
+        Topic.create!(
+          title: 'Test Topic',
+          category_id: category.id
+        )
+      end
+
+      let!(:deck) do
+        Deck.create!(
+          title: 'Test Deck',
+        )
+      end
+
+      let!(:card) do
+        Card.create!(
+          title: 'Test Card',
+          face_content: 'This is the content on the face (lined) of the index card.',
+          back_content: 'The back side of the card is the blank side.'
+        )
+      end
+
+      it 'returns the content on the face of the card' do
+        get "/cards/#{card.id}?side=back"
+        expect(response.body).to include('The back side of the card is the blank side.')
+      end
   end
 end
