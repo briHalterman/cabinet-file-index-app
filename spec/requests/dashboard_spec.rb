661c50ebcd455fba1d0f8bde5b84b4cdd62975d3
Brittany Halterman <halterma@gmail.com>
2025-09-28 12:23:11 -0400

    Outline test cases

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -6,5 +6,11 @@ RSpec.describe "Dashboard", type: :request do
       get "/dashboard"
       expect(response).to have_http_status(:ok)
     end
+
+    it 'displays links to categories' do
+    end
+
+    it 'displays links to the most recently edited topics in each category' do
+    end
   end
 end
