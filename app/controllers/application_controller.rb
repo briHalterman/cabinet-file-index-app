13acb7ab66347c78d1de763b9d83df1e66faf658
Brittany Halterman <halterma@gmail.com>
2025-10-06 15:47:46 -0400

    Implement the create action for the form submission

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  def require_user
+    if session[:role] != 'user'
+      flash[:alert] = 'You do not have access tot that page'
+        redirect_do login_path
+      end
+  end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class DashboardController < ApplicationController
+  before_action :require_user
+
   def index
     @categories = Category.all
   end
