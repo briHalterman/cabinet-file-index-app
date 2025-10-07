6239e903a5850f4133d20ea0554c877e775df8a3
Brittany Halterman <halterma@gmail.com>
2025-10-07 15:41:12 -0400

    Create and log in user to access restricted routes in tests

--- spec/requests/cards_spec.rb
+++ spec/requests/cards_spec.rb
@@ -2,95 +2,127 @@ require 'rails_helper'
 
 RSpec.describe "Cards", type: :request do
   describe "GET /cards" do
-    it 'responds with 200 OK' do
-      get "/cards"
-      expect(response).to have_http_status(:ok)
-    end
-
-    context 'categories exist' do
-      let!(:category) do
-        Category.create!(
-          title: 'Test Category'
-        )
-      end
-
-      let!(:topic) do
-        Topic.create!(
-          title: 'Test Topic',
-          category_id: category.id
-        )
-      end
-
-      let!(:deck) do
-        Deck.create!(
-          title: 'Test Deck'
+    context 'user is logged on' do
+      let!(:user) do
+        User.create!(
+          username: 'User',
+          password: 'secret',
+          role: 'user'
         )
       end
 
       before do
-        topic.decks << deck
+        post '/login', params: {
+          username: user.username,
+          password: 'secret'
+        }
       end
 
-      let!(:card1) do
-        Card.create!(
-          title: 'Test Card 1'
-        )
+      it 'responds with 200 OK' do
+        get "/cards"
+        expect(response).to have_http_status(:ok)
       end
 
-      let!(:card2) do
-        Card.create!(
-          title: 'Test Card 2'
-        )
+      context 'categories exist' do
+        let!(:category) do
+          Category.create!(
+            title: 'Test Category'
+          )
+        end
+
+        let!(:topic) do
+          Topic.create!(
+            title: 'Test Topic',
+            category_id: category.id
+          )
+        end
+
+        let!(:deck) do
+          Deck.create!(
+            title: 'Test Deck'
+          )
+        end
+
+        before do
+          topic.decks << deck
+        end
+
+        let!(:card1) do
+          Card.create!(
+            title: 'Test Card 1'
+          )
+        end
+
+        let!(:card2) do
+          Card.create!(
+            title: 'Test Card 2'
+          )
+        end
+
+        before do
+          deck.cards << card1
+          deck.cards << card2
+        end
+
+        it 'returns a page containing titles of all cards' do
+          get '/cards'
+          expect(response.body).to include('Test Card 1')
+          expect(response.body).to include('Test Card 2')
+        end
       end
 
-      before do
-        deck.cards << card1
-        deck.cards << card2
-      end
-
-      it 'returns a page containing titles of all cards' do
-        get '/cards'
-        expect(response.body).to include('Test Card 1')
-        expect(response.body).to include('Test Card 2')
+      context 'categories do not exist' do
+        it 'returns a page containing no names of cards' do
+          get '/cards'
+          expect(response.body).to include('All Cards')
+          # expect(response.body).not_to include('<li>')
+        end
       end
     end
-
-    context 'categories do not exist' do
-      it 'returns a page containing no names of cards' do
-        get '/cards'
-        expect(response.body).to include('All Cards')
-        expect(response.body).not_to include('<li>')
-      end
-    end
-
   end
 
   describe "GET /decks/:id/cards/:id?side=face" do
-      let!(:category) do
-        Category.create!(
-          title: 'Test Category'
+    let!(:category) do
+      Category.create!(
+        title: 'Test Category'
+      )
+    end
+
+    let!(:topic) do
+      Topic.create!(
+        title: 'Test Topic',
+        category_id: category.id
+      )
+    end
+
+    let!(:deck) do
+      Deck.create!(
+        title: 'Test Deck',
+      )
+    end
+
+    let!(:card) do
+      Card.create!(
+        title: 'Test Card',
+        face_content: 'This is the content on the face (lined) of the index card.',
+        back_content: 'The back side of the card is the blank side.'
+      )
+    end
+
+    context 'user is logged in' do
+      let!(:user) do
+        User.create!(
+          username: 'User',
+          password: 'secret',
+          role: 'user'
         )
       end
 
-      let!(:topic) do
-        Topic.create!(
-          title: 'Test Topic',
-          category_id: category.id
-        )
-      end
-
-      let!(:deck) do
-        Deck.create!(
-          title: 'Test Deck',
-        )
-      end
-
-      let!(:card) do
-        Card.create!(
-          title: 'Test Card',
-          face_content: 'This is the content on the face (lined) of the index card.',
-          back_content: 'The back side of the card is the blank side.'
-        )
+      before do
+        post '/login', params: {
+          username: user.username,
+          password: 'secret'
+        }
       end
 
       it 'returns the title of the deck' do
@@ -102,6 +134,7 @@ RSpec.describe "Cards", type: :request do
         get "/decks/#{deck.id}/cards/#{card.id}?side=face"
         expect(response.body).to include('This is the content on the face (lined) of the index card.')
       end
+    end
   end
 
   describe "GET /decks/:id/cards/:id?side=back" do
@@ -132,14 +165,31 @@ RSpec.describe "Cards", type: :request do
       )
     end
 
-    it 'returns the title of the deck' do
-      get "/decks/#{deck.id}/cards/#{card.id}?side=back"
-      expect(response.body).to include('Test Deck')
-    end
+    context 'user is logged in' do
+      let!(:user) do
+        User.create!(
+          username: 'User',
+          password: 'secret',
+          role: 'user'
+        )
+      end
 
-    it 'returns the content on the face of the card' do
-      get "/decks/#{deck.id}/cards/#{card.id}?side=back"
-      expect(response.body).to include('The back side of the card is the blank side.')
+      before do
+        post '/login', params: {
+          username: user.username,
+          password: 'secret'
+        }
+      end
+
+      it 'returns the title of the deck' do
+        get "/decks/#{deck.id}/cards/#{card.id}?side=back"
+        expect(response.body).to include('Test Deck')
+      end
+
+      it 'returns the content on the face of the card' do
+        get "/decks/#{deck.id}/cards/#{card.id}?side=back"
+        expect(response.body).to include('The back side of the card is the blank side.')
+      end
     end
   end
 
@@ -147,8 +197,8 @@ RSpec.describe "Cards", type: :request do
     let!(:category) do
       Category.create!(
         title: 'Test category'
-      )
-    end
+        )
+      end
 
     let!(:topic) do
       Topic.create!(
@@ -167,17 +217,35 @@ RSpec.describe "Cards", type: :request do
       topic.decks << deck
     end
 
-    it 'displays the title, face content, back content and deck labels' do
-      get "/decks/#{deck.id}/cards/new"
+    context 'user is logged in' do
+      let!(:user) do
+        User.create(
+          username: 'User',
+          password: 'secret',
+          role: 'user'
+        )
+      end
 
-      expect(response.body).to include('Title')
-      expect(response.body).to include('Face content')
-      expect(response.body).to include('Back content')
-      expect(response.body).to include('Deck')
+      before do
+        post '/login', params: {
+          username: user.username,
+          password: 'secret'
+        }
+      end
+
+      it 'displays the title, face content, back content and deck labels' do
+        get "/decks/#{deck.id}/cards/new"
+
+        expect(response.body).to include('Title')
+        expect(response.body).to include('Face content')
+        expect(response.body).to include('Back content')
+        expect(response.body).to include('Deck')
+      end
     end
   end
 
   describe 'POST /decks/:id/cards' do
+
     let(:category) do
       Category.create!(
         title: 'Test category'
@@ -201,39 +269,56 @@ RSpec.describe "Cards", type: :request do
       topic.decks << deck
     end
 
-    it "creates a new card when title and deck exist and are valid" do
-      post "/decks/#{deck.id}/cards", params: {
-        card: {
-          title: 'New card',
-          deck_id: deck.id
+    context 'user is logged in' do
+      let!(:user) do
+        User.create!(
+          username: 'User',
+          password: 'secret',
+          role: 'user'
+        )
+      end
+
+      before do
+        post '/login', params: {
+          username: user.username,
+          password: 'secret'
         }
-      }
+      end
 
-      expect(response).to redirect_to(deck_card_path(deck.id, Card.last.id))
-
-      expect(Card.last.title).to eq('New card')
-      expect(Card.last.decks.first.title).to eq('Test deck')
-    end
-
-    it 'responds with 400 status when no deck is provided' do
-      post "/decks/#{deck.id}/cards", params: {
-        card: {
-          title: 'New card'
+      it "creates a new card when title and deck exist and are valid" do
+        post "/decks/#{deck.id}/cards", params: {
+          card: {
+            title: 'New card',
+            deck_id: deck.id
+          }
         }
-      }
 
-      expect(response).to have_http_status(:bad_request)
-    end
+        expect(response).to redirect_to(deck_card_path(deck.id, Card.last.id))
 
-    it 'responds with 400 status when deck id does not belong to an existing deck' do
-      post "/decks/#{deck.id}/cards", params: {
-        card: {
-          title: 'New card',
-          deck_id: 'nope'
+        expect(Card.last.title).to eq('New card')
+        expect(Card.last.decks.first.title).to eq('Test deck')
+      end
+
+      it 'responds with 400 status when no deck is provided' do
+        post "/decks/#{deck.id}/cards", params: {
+          card: {
+            title: 'New card'
+          }
         }
-      }
 
-      expect(response).to have_http_status(:bad_request)
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'responds with 400 status when deck id does not belong to an existing deck' do
+        post "/decks/#{deck.id}/cards", params: {
+          card: {
+            title: 'New card',
+            deck_id: 'nope'
+          }
+        }
+
+        expect(response).to have_http_status(:bad_request)
+      end
     end
   end
 
@@ -273,34 +358,51 @@ RSpec.describe "Cards", type: :request do
       deck.cards << card
     end
 
-    it 'responds with 200 OK' do
-      get "/decks/#{deck.id}/cards/#{card.id}/edit"
+    context 'user is logged in' do
+      let!(:user) do
+        User.create!(
+          username: 'User',
+          password: 'secret',
+          role: 'user'
+        )
+      end
 
-      expect(response).to have_http_status(:ok)
-    end
+      before do
+        post '/login', params: {
+          username: user.username,
+          password: 'secret'
+        }
+      end
 
-    it 'displays the title label' do
-      get "/decks/#{deck.id}/cards/#{card.id}/edit"
+      it 'responds with 200 OK' do
+        get "/decks/#{deck.id}/cards/#{card.id}/edit"
 
-      expect(response.body).to include('Title')
-    end
+        expect(response).to have_http_status(:ok)
+      end
 
-    it 'displays the face content label' do
-      get "/decks/#{deck.id}/cards/#{card.id}/edit"
+      it 'displays the title label' do
+        get "/decks/#{deck.id}/cards/#{card.id}/edit"
 
-      expect(response.body).to include('Face content')
-    end
+        expect(response.body).to include('Title')
+      end
 
-    it 'displays the back content label' do
-      get "/decks/#{deck.id}/cards/#{card.id}/edit"
+      it 'displays the face content label' do
+        get "/decks/#{deck.id}/cards/#{card.id}/edit"
 
-      expect(response.body).to include('Back content')
-    end
+        expect(response.body).to include('Face content')
+      end
 
-    it 'displays the deck label' do
-      get "/decks/#{deck.id}/cards/#{card.id}"
+      it 'displays the back content label' do
+        get "/decks/#{deck.id}/cards/#{card.id}/edit"
 
-      expect(response.body).to include('Deck')
+        expect(response.body).to include('Back content')
+      end
+
+      it 'displays the deck label' do
+        get "/decks/#{deck.id}/cards/#{card.id}"
+
+        expect(response.body).to include('Deck')
+      end
     end
   end
 
@@ -347,52 +449,69 @@ RSpec.describe "Cards", type: :request do
       deck1.cards << card
     end
 
-    it "updates a card's title, content, and/or deck when deck is valid and card exists" do
-      put "/decks/#{deck1.id}/cards/#{card.id}", params: {
-        card: {
-          title: 'New title',
-          deck_id: deck2.id,
-          face_content: 'Edited content on the face of the card',
-          back_content: 'Edited content on the back of the card'
+    context 'user is logged in' do
+      let!(:user) do
+        User.create!(
+          username: 'User',
+          password: 'secret',
+          role: 'user'
+        )
+      end
+
+      before do
+        post '/login', params: {
+          username: user.username,
+          password: 'secret'
         }
-      }
-
-      expect(response).to redirect_to(deck_card_path(deck2.id, Card.last.id))
-      card.reload
-      expect(Card.last.title).to eq('New title')
-      expect(Card.last.decks.first.title).to eq('Test deck 2')
-    end
-
-    it 'responds with 400 status when deck id is not provided' do
-      put "/decks/#{deck1.id}/cards/#{card.id}", params: {
-        card: {
-          title: 'New title'
+      end
+      
+      it "updates a card's title, content, and/or deck when deck is valid and card exists" do
+        put "/decks/#{deck1.id}/cards/#{card.id}", params: {
+          card: {
+            title: 'New title',
+            deck_id: deck2.id,
+            face_content: 'Edited content on the face of the card',
+            back_content: 'Edited content on the back of the card'
+          }
         }
-      }
 
-      expect(response).to have_http_status(:bad_request)
-    end
+        expect(response).to redirect_to(deck_card_path(deck2.id, Card.last.id))
+        card.reload
+        expect(Card.last.title).to eq('New title')
+        expect(Card.last.decks.first.title).to eq('Test deck 2')
+      end
 
-    it 'responds with 400 status when deck id does not belong to a valid deck' do
-      put "/decks/#{deck1.id}/cards/#{card.id}", params: {
-        card: {
-          title: 'New title',
-          deck_id: 'nope'
+      it 'responds with 400 status when deck id is not provided' do
+        put "/decks/#{deck1.id}/cards/#{card.id}", params: {
+          card: {
+            title: 'New title'
+          }
         }
-      }
 
-      expect(response).to have_http_status(:bad_request)
-    end
+        expect(response).to have_http_status(:bad_request)
+      end
 
-    it 'responds with 404 status when card id does not belong to an existing card' do
-      put "/decks/#{deck1.id}/cards/nope", params: {
-        card: {
-          title: 'New title',
-          deck_id: deck2.id
+      it 'responds with 400 status when deck id does not belong to a valid deck' do
+        put "/decks/#{deck1.id}/cards/#{card.id}", params: {
+          card: {
+            title: 'New title',
+            deck_id: 'nope'
+          }
         }
-      }
 
-      expect(response).to have_http_status(:not_found)
+        expect(response).to have_http_status(:bad_request)
+      end
+
+      it 'responds with 404 status when card id does not belong to an existing card' do
+        put "/decks/#{deck1.id}/cards/nope", params: {
+          card: {
+            title: 'New title',
+            deck_id: deck2.id
+          }
+        }
+
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
 end
