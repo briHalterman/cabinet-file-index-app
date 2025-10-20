247cd67671a0811cd4c9af12227009de1c216344
Brittany Halterman <halterma@gmail.com>
2025-10-20 15:33:53 -0400

    Implement users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,20 @@ class UsersController < ApplicationController
     @user.role = 'user'
   end
 
-  def create; end
+  def create
+    @user = User.new(user_params)
+    @user.role = 'user'
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to login_path, notice: 'New user was successfully registered. Please log in.' }
+        format.json { render :login, status: :created, location: login_path }
+      else
+        format.html { render :new, status: :bad_request }
+        format.json { render json: @user.errors, status: :bad_request }
+      end
+    end
+  end
 
   def edit; end
 
