d641498bf0f0d61e8667ee348540d39f5bbc4d4e
Brittany Halterman <halterma@gmail.com>
2025-10-20 09:23:19 -0400

    Add associations to models

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,4 +3,6 @@ class Topic < ApplicationRecord
   has_many :decks, through: :deck_topics
 
   belongs_to :category
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_many :topics
   has_many :decks
   has_many :cards
 
