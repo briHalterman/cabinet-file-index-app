27dab042505dd9ab45a36a9c9c6cecb6553f51df
Brittany Halterman <halterma@gmail.com>
2025-10-21 12:06:21 -0400

    Debug dashboard to scope only to current user's topics

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,8 @@ class DashboardController < ApplicationController
   before_action :require_user
 
   def index
+    @current_user = User.find_by(id: session[:user_id])
     @categories = Category.all
+    # @topics = Topic.where()
   end
 end
